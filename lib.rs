d09c59deae53ec23bb65973126333d404f756de5
Stefan Schindler <stefan@estada.ch>
2016-10-17 12:42:04 +0200

    Mention the synchronisation.

--- lib.rs
+++ lib.rs
@@ -40,7 +40,7 @@
 //!
 //! ## Syncronized with a barrier
 //!
-//! Keep in mind, if you put more jobs in the pool than you have workers,
+//! Keep in mind, if a barrier synchronizes more jobs than you have workers in the pool,
 //! you will end up with a [deadlock](https://en.wikipedia.org/wiki/Deadlock)
 //! at the barrier which is [not considered unsafe]
 //! (http://doc.rust-lang.org/reference.html#behavior-not-considered-unsafe).
